3ccd3095f5251938d2d237c1ec0421b38fba4580
JordiLara <165904939+JordiLara@users.noreply.github.com>
2024-07-30 12:38:19 +0200

    add small changes for better visual recognition of elements

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,5 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
+// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional.
+
 var products = [
     {
         id: 1,
@@ -70,12 +71,15 @@ var products = [
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
+
 var cart = [];
 
 var total = 0;
 
 // Exercise 1
+
 function buy(id) {
+
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array
 
@@ -94,21 +98,26 @@ function buy(id) {
 }
 
 // Exercise 2
+
 function cleanCart() {
 }
 
 // Exercise 3
+
 function calculateTotal() {
+
     // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
 function applyPromotionsCart() {
+
     // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 5
 function printCart() {
+
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
 
@@ -116,6 +125,7 @@ function printCart() {
 // ** Nivell II **
 
 // Exercise 7
+
 function removeFromCart(id) {
 
 }
